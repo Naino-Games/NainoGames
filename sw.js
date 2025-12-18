15a8ecaa4ba6c7a325680c669d2619fd1f42faaa
Nawab Husnain <nainogame@gmail.com>
2025-12-19 00:35:00 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10345521
+    "zoneId": 10346005
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
